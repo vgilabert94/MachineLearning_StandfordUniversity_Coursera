929b390405965a14a763e6d6bc21a80391a82f9b
vgilabert94 <44602177+vgilabert94@users.noreply.github.com>
2020-04-26 19:20:18 +0200

    Add files via upload

---
+++ ex7/findClosestCentroids.m
@@ -0,0 +1,36 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+m = size(X,1);
+% You need to return the following variables correctly.
+idx = zeros(m, 1);
+dist = zeros(m,K);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+for i=1:m % Loop for every example X(m).
+    for k=1:K %Loop for every cluster k.
+        %Calculate the distance between X(i) and centroid(k).
+        diff = (X(i,:) - centroids(k,:)).^2;
+        dist(i,k)= sqrt(sum(diff));
+    end
+    %Obtain de min value for every m, and asig the index.
+    [~,idx(i)] = min(dist(i,:));
+end
+% =============================================================
+
+end
+
